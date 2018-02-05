63f6b2e46a8d8e530eb81b52e2807e851133aeee
princemathew <mailprincemathew@gmail.com>
2018-02-05 22:25:06 +0530

    updated server.js

--- masternode/server.js
+++ masternode/server.js
@@ -5,8 +5,10 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 var authorizationController=require('./server/controllers/authorization')
+var ipfscontroller=require('./server/controllers/postipfs')
 
 app.post('/api/patient/register',authorizationController.register)
+app.post('/api/ipfs/add',ipfscontroller.addData)
 app.listen(3010,()=>{
     console.log('server listening at 3010')
 })
