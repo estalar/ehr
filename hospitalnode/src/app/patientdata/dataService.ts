0cade842b9444fb4283f13ef1f0d2bd74c59ac37
alvinzach <alvinzach@gmail.com>
2018-02-06 19:27:54 +0530

    updates

--- hospitalnode/src/app/patientdata/dataService.ts
+++ hospitalnode/src/app/patientdata/dataService.ts
@@ -1,9 +1,16 @@
 import {Http} from '@angular/http'
 import {Injectable} from '@angular/core'
+import { RequestOptions } from '@angular/http/src/base_request_options';
 @Injectable() export class dataService{
     result:any
     constructor(private _http:Http){
 
     }
-    return this._http.post('/api/ipfs/get').map(result=>this.result=result.json().data)
+    getdata(data){
+        let headers=new Headers({
+            'Content-Type':'application/json'
+        })
+        let options=new RequestOptions({headers:headers})
+        return this._http.post('/api/ipfs/get',JSON.stringify(data)).map
+    }
 }
--- masternode/smartcontracts/deploycontracts.js
+++ masternode/smartcontracts/deploycontracts.js
@@ -2,22 +2,16 @@ const fs = require('fs');
 const solc = require('solc');
 const Web3 = require('web3');
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-const input = fs.readFileSync('patient.sol');
+const input = fs.readFileSync('master.sol');
 const output = solc.compile(input.toString(), 1);
-const bytecode = output.contracts[':patient'].bytecode;
-const abi = JSON.parse(output.contracts[':patient'].interface);
-console.log(output.contracts[':patient'].interface)
+const bytecode = output.contracts[':Master'].bytecode;
+const abi = JSON.parse(output.contracts[':Master'].interface);
+console.log(output.contracts[':Master'].interface)
 const contract = web3.eth.contract(abi);
-<<<<<<< HEAD
-//web3.personal.unlockAccount(web3.eth.accounts[0],"kattanam",100000)
-
-const contractInstance = contract.new("29/08/1995",{
-=======
 web3.personal.unlockAccount(web3.eth.accounts[0],"kattanam",100000)
 const contractInstance = contract.new({
->>>>>>> 1433483f5dbf3fd715c98b8f551d3b601043d62c
     data: '0x' + bytecode,
     from: web3.eth.accounts[0],
     gas: 90000*5
@@ -33,7 +27,9 @@ const contractInstance = contract.new({
         if(contractInstance.address!==undefined){
             console.log(contractInstance.address)
             clearInterval(intId)
-            contractInstance.address=undefined
+            contractInstance.address=undefined         
         }
     },3000)
 });
+
+
